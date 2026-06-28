fa75df4a30f9d55e08f95b1c564240dc6fcef7f3
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-06-28 15:53:01 +0530

    feat: Implement user retrieval handler and restructure HTTP server setup

--- cmd/go-api-server/main.go
+++ cmd/go-api-server/main.go
@@ -1,26 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
-
-type server struct {
+type api struct{
 	addr string
 }
 
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Hello "))	
+func (a * api) getUserHandler(w http.ResponseWriter, r *http.Request){
+	// w.Write([] byte("hello"))
+	fmt.Fprintf(w,"hello br")
 }
 
 
 func main(){
-	s:=&server{addr: ":8080"}
-	err:=http.ListenAndServe(s.addr,s)
-	if err !=nil{
+	api:=&api{addr: ":8080"}
+	mux:= http.NewServeMux()
+
+
+	srv:=&http.Server{
+		Addr: api.addr,
+		Handler: mux,
+	}
+	mux.HandleFunc("GET /users", api.getUserHandler)
+
+	err:= srv.ListenAndServe()
+	if err!=nil{
 		log.Fatal(err)
 	}
 
+	
 }
+
+
