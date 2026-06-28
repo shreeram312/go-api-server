79df9d948dbf4e4e8be83cf54ee5e55642cd3cf3
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-06-29 00:10:24 +0530

    feat: Implement user creation handler with JSON validation and conflict resolution in API server

--- cmd/go-api-server/main.go
+++ cmd/go-api-server/main.go
@@ -19,18 +19,50 @@ type User struct{
 
 var users = []User{}
 
+
+func (a *api) createUserHandler(w http.ResponseWriter, r * http.Request){
+	var user User
+
+err:=	json.NewDecoder(r.Body).Decode(&user)
+
+	if err !=nil{
+		http.Error(w,"Invalid json",http.StatusBadRequest)
+	}	
+
+	for _,existingUser := range users{
+		if existingUser.FirstName == user.FirstName &&
+		existingUser.LastName == user.LastName {
+	
+		http.Error(w, "User already exists", http.StatusConflict)
+		return
+	}
+	}  
+	users = append(users, user)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	err = json.NewEncoder(w).Encode(user)
+	if err != nil {
+	http.Error(w, err.Error(), http.StatusInternalServerError)
+	return
+	}
+
+}
+
 func (a * api) getUserHandler(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("content-type","application/json")
 
 
+	
+	
+
+	w.WriteHeader(http.StatusOK)
 	err:=json.NewEncoder(w).Encode(users)
 	if err!=nil{
 		http.Error(w,err.Error(),http.StatusInternalServerError)
 		return
 	}
 
-
-	w.WriteHeader(http.StatusOK)
 	// w.Write([] byte("hello"))
 	// fmt.Fprintf(w,"hello br")
 	// both are allowed 
@@ -47,6 +79,7 @@ func main(){
 		Handler: mux,
 	}
 	mux.HandleFunc("GET /users", api.getUserHandler)
+	mux.HandleFunc("POST /users", api.createUserHandler)
 
 	err:= srv.ListenAndServe()
 	if err!=nil{
